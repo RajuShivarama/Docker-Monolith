f60e09b8b3e73ac743b04f90a241c83d57d46365
RajuShivarama <rajushivaram97@gmail.com>
2023-08-08 19:16:56 +0530

    django monolith and mern monolith

---
+++ Monolith Djnago and React/react-ambassador/next-checkout/constants.ts
@@ -0,0 +1,4 @@
+export default {
+    endpoint: 'http://localhost:8000/api/checkout',
+    stripe_key: 'pk_test_51L5to5SEu11LyC0t5QOnfU5pTWjeb0SJYyGal4qMutRuPQ0kvYZSX2N8rcMpNgnvg6MQRAVDB4AsOpSYoG5Kejpw00KBCkP3as'
+}
